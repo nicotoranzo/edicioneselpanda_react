b72eec7000fedec09a98e246320fdf51203db0ec
Nicolas Toranzo <toranzo.nicolas@gmail.com>
2023-05-23 14:31:21 -0300

    entrega final

--- src/services/FirebaseConfig.jsx
+++ src/services/FirebaseConfig.jsx
@@ -1,16 +1,16 @@
 
 import {getFirestore} from "firebase/firestore"
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCu3TXEC0YkukzbuCGk6phzPbHuZCYvKNw",
-  authDomain: "edicioneselpanda-edf76.firebaseapp.com",
-  projectId: "edicioneselpanda-edf76",
-  storageBucket: "edicioneselpanda-edf76.appspot.com",
-  messagingSenderId: "26142110350",
-  appId: "1:26142110350:web:d888b0efc1b075420f8fdf"
-};
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+}
 
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
